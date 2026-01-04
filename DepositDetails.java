464066b1147c6e76d5de93830cf4d5d996fe051a
Yasmin Ayuni <ysmnayn04@gmail.com>
2026-01-04 19:55:57 +0800

    change size value

--- DepositDetails.java
+++ DepositDetails.java
@@ -16,7 +16,7 @@ public class DepositDetails extends JFrame implements ActionListener{
         
         super("Deposit Details");
         
-        setSize(700,700);
+        setSize(600,700); // changed by yasmin
         setLocation(400,100);
         
         getContentPane().setBackground(Color.white);
